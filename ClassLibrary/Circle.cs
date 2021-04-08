23e27dc17bee48f58c77050182cd8b5306fec963
Rickstaper <karchevskji@gmail.com>
2021-04-08 19:38:11 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary/Circle.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1
+{
+    public class Circle
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public Point Center { get; set; }
+        public double Radius { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="point"></param>
+        /// <param name="radius"></param>
+        public Circle(Point point, double radius)
+        {
+            Center = point;
+            Radius = radius;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public double Area()
+        {
+            return Math.PI * Math.Pow (Radius, 2);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public double Perimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="point"></param>
+        /// <returns></returns>
+        public bool Validation(Point point)
+        {
+            // Distance between center and target point
+            double x = Math.Sqrt (Math.Pow (point.X - Center.X, 2) + Math.Pow (point.Y - Center.Y, 2);
+
+            if (x > Radius)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ ClassLibrary/Triangle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1
+{
+    /// <summary>
+    /// Flat figure representation - triangle
+    /// </summary>
+    public class Triangle
+    {
+        Point A, B, C;
+        double AB, BC, AC;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="point1">A point</param>
+        /// <param name="point2">B point</param>
+        /// <param name="point3">C point</param>
+        public Triangle (Point point1, Point point2, Point point3)
+        {
+            A = point1;
+            B = point2;
+            C = point3;
+
+            AB = Math.Sqrt (Math.Pow (B.X - A.X, 2) + Math.Pow (B.Y - A.Y, 2));
+            BC = Math.Sqrt (Math.Pow (C.X - B.X, 2) + Math.Pow (C.Y - B.Y, 2));
+            AC = Math.Sqrt (Math.Pow (C.X - A.X, 2) + Math.Pow (C.Y - A.Y, 2));
+        }
+
+        public string Sides ()
+        {
+            return $"AB:{AB}\nBC:{BC}\nAC:{AC}";
+        }
+
+        public double Perimeter()
+        {
+            return AB + BC + AC;
+        }
+
+        public double Area()
+        {
+            double p = Perimeter () / 2.0;
+            double h = (2*Math.Sqrt(p*(p-AC)*(p-AB)*(p-BC))) / AC;
+            double s = (AC * h) / 2.0;
+            return s;
+        }
+
+        public bool PointBelongs(Point point)
+        {
+            double first = (A.X - point.X) * (B.Y - A.Y) - (B.X - A.X) * A.Y - point.Y;
+            double second = (B.X - point.X) * (C.Y - B.Y) - (C.X - B.X) * B.Y - point.Y;
+            double third = (C.X - point.X) * (A.Y - C.Y) - (A.X - C.X) * C.Y - point.Y;
+
+            if ((first >= 0 && second >= 0 && third >= 0) || (first <= 0 && second <= 0 && third <= 0))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Validation()
+        {
+            if (AB + BC <= AC || AB+AC <= BC || AC + BC <= AB)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+    }
+}
